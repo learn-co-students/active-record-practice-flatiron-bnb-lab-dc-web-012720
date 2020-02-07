6f1bc74808b25352bd23e5b87e74bfce33d5d5d2
vplacido <valentinplacido25@gmail.com>
2020-02-07 15:48:01 -0500

    passed all test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
     has_many :reservations, through: :listings, :foreign_key => "host_id"
 
     #Guests
-    has_many :reservations, through: :listings, :foreign_key => "guest_id"
+    has_many :trips, class_name: "Reservation", :foreign_key => "guest_id"
     has_many :reviews, foreign_key: "guest_id"
 end
