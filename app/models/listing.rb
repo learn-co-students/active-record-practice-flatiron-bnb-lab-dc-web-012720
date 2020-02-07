9d867d6846204149fcb1a6ec96087280d17a5206
rjwilson0531 <rjwilson0531@gmail.com>
2020-02-07 15:27:13 -0500

    almost done

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,8 @@
 class Listing < ActiveRecord::Base 
-    belongs_to :user 
-    has_many :cities
+    belongs_to :host, :class_name => "User" 
+    belongs_to :neighborhood
+    belongs_to :city
+    has_many :reservations
+    has_many :guests, through: :reservations
+    has_many :reviews, through: :reservations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base 
-    has_many :listings, class_name: "User", foreign_key: "host_id"
-    has_many :reservations, class_name: "User", foreign_key: "guest_id"
-    has_many :reviews, class_name: "User", foreign_key: "guest_id"
+
+    #Hosts
+    has_many :listings, foreign_key: "host_id"
+    has_many :reservations, through: :listings, foreign_key: "host_id"
+
+    #Guests
+    has_many :reservations, foreign_key: "guest_id"
+    has_many :reviews, foreign_key: "guest_id"
 end
