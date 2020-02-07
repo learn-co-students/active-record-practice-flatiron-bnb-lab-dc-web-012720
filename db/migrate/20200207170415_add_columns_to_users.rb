e3e25f8ce6c897781cd3a064cfaaa84df6743b5c
rjwilson0531 <rjwilson0531@gmail.com>
2020-02-07 14:53:43 -0500

    deleted last migration

--- db/migrate/20200207170415_add_columns_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :host_id, :integer
-    add_column :users, :guest_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200207170415) do
+ActiveRecord::Schema.define(version: 20200207165818) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
@@ -46,9 +46,7 @@ ActiveRecord::Schema.define(version: 20200207170415) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string  "name"
-    t.integer "host_id"
-    t.integer "guest_id"
+    t.string "name"
   end
 
 end
