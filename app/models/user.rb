e697f48940a19229608cf94ad114fc1bf16dcab9
github username <github email address>
2020-02-07 14:17:25 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ActiveRecord::Base
     has_many :reviews, :foreign_key => 'guest_id'
-    has_many :reservations, :foreign_key => 'guest_id'
+    has_many :trips, :class_name => "Reservation", :foreign_key => 'guest_id'
     has_many :listings, :foreign_key => 'host_id'
     has_many :reservations, through: :listings 
-  
-    def trips 
-        binding.pry 
-        self.reservations
-    end 
 end 
